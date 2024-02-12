4dcf27ee7133dfa3dda273be387b1c87baeaa5cb
Denitria <dogma5545@yandex.ru>
2024-02-12 14:28:21 +0300

    Спринт 10

---
+++ app/src/main/java/com/trial/playlistmaker/TrackAdapter.kt
@@ -0,0 +1,52 @@
+package com.trial.playlistmaker
+
+import android.content.Context
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TrackAdapter (private val data: List<Track>) : RecyclerView.Adapter<TrackAdapter.TrackViewHolder> () {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+        return TrackViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val trackName: TextView = itemView.findViewById(R.id.trackName)
+        private val trackImage: ImageView = itemView.findViewById(R.id.trackImage)
+        private val trackTime: TextView = itemView.findViewById(R.id.trackTime)
+        private val trackArtist: TextView = itemView.findViewById(R.id.artistName)
+
+        fun bind(model: Track) {
+            trackName.text = model.trackName
+            trackTime.text = model.trackTime
+            trackArtist.text = model.artistName
+            Glide.with(this.itemView.context)
+                .load(model.artworkUrl100)
+                .fitCenter().dontAnimate()
+                .placeholder(R.drawable.placeholder)
+                .transform(RoundedCorners(dpToPx(2f, itemView.context))).into(trackImage)
+        }
+
+        private fun dpToPx(dp: Float, context: Context): Int {
+            return TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics
+            ).toInt()
+        }
+    }
+}
